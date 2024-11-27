87466d2b75e7a67edcdf8eeac7386ef9c6f9f9ae
Abdullah Almamun <mamun2social@gmail.com>
2024-11-27 21:39:26 +0600

    vue tutorial 3rd video completed
    video link: https://www.youtube.com/watch?v=CYPZBK8zUik

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
             showIt: true,
             title: 'The Ottoman Empire',
             sultan: 'Sultan Mehmet',
-            age: 49
+            age: 49,
+            x: 0,
+            y: 0,
+            books: [
+                { name: 'Bishad Sindhu', author: 'Mir Musharraf Hossain', img: 'https://placehold.co/50x30/orange/white', isFav: true, },
+                { name: 'Love Candy', author: 'Unknown', img: 'https://placehold.co/50x30/orange/black', isFav: true, },
+                { name: 'Meghnadhbodh', author: 'Micheal Modhusudan Dutt', img: 'https://placehold.co/50x30/black/white', isFav: false, },
+                { name: 'Gitanjoli', author: 'Robindra nath Thakur', img: 'https://placehold.co/50x30/black/orange', isFav: true, },
+            ],
+            url: 'https://almamun.life',
         }
     },
     methods: {
-        changeTitle(newTitle){
+        changeTitle(newTitle) {
             // this.title = 'Constantinopole'
             this.title = newTitle
         },
-        toggleShow(){
+        toggleShow() {
             this.showIt = !this.showIt
+        },
+        handleEvent(e, data) {
+            console.log(e)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMouse(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        toggleFavourite(book) {
+            book.isFav = !book.isFav
+        }
+    },
+    computed:{
+        filteredBook(){
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
